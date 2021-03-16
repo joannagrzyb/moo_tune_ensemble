e059ae8a0a8845484c37edc5a876ee2dcb8c87a6
joannagrzyb <joanna.grzyb@pwr.edu.pl>
2021-03-16 19:59:07 +0100

    fix

--- plgrid_run.sh
+++ plgrid_run.sh
@@ -6,7 +6,6 @@
 #SBATCH -A plgjoannagrzyb2021a
 
 
-cd $SLURM_SUBMIT_DIR
 pip3 install -r requirements.txt --user
 pip3 install -U stream-learn
 python3 -W ignore python experiment1_9higher_part1.py
