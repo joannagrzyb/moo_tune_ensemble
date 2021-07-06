25fffe13f20ec8874219b846a334799dcd81d5b8
joannagrzyb <joanna.grzyb@pwr.edu.pl>
2021-07-06 15:43:53 +0200

    FS

--- analysis4_9lower.py
+++ analysis4_9lower.py
@@ -156,7 +156,7 @@ def horizontal_bar_chart():
 # pairs_metrics_multi_grid(method_names=methods_alias, data_np=data_np, experiment_name="experiment_server/experiment4_9lower", dataset_names=datasets, metrics=metrics_alias, filename="ex9l_ranking_plot_grid_variants", ref_methods=methods_alias[0:3], offset=-25)
 
 # Wilcoxon ranking grid - statistic test for all methods vs: SEMOOS, SEMOOSb, SEMOOSbp and all metrics
-# pairs_metrics_multi_grid_all(method_names=methods_alias, data_np=data_np, experiment_name="experiment_server/experiment4_9lower", dataset_names=datasets, metrics=metrics_alias, filename="ex9l_ranking_plot_grid_all", ref_methods=methods_alias[0:3], offset=-25)
+pairs_metrics_multi_grid_all(method_names=methods_alias, data_np=data_np, experiment_name="experiment_server/experiment4_9lower", dataset_names=datasets, metrics=metrics_alias, filename="ex9l_ranking_plot_grid_all", ref_methods=methods_alias[0:3], offset=-25)
 
 # Diversity bar Plotting
-diversity_bar_plot(diversity_mean, diversity_measures, methods_alias[:4], experiment_name="experiment4_9lower")
+# diversity_bar_plot(diversity_mean, diversity_measures, methods_alias[:4], experiment_name="experiment4_9lower")
